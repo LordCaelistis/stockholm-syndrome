ae8afad8dbd67c4cd0cf6144c0b6c9cf11893c64
the Sÿhl <syhl.neo@gmail.com>
2018-12-07 05:24:03 +0100

    Sons d'atterissage + optimisation code son footsteps

--- Assets/Programmation/Déplacement/Input_Jump.cs
+++ Assets/Programmation/Déplacement/Input_Jump.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +7,13 @@ public class Input_Jump : MonoBehaviour {
     // public GameObject personnage;
     public KnightController scriptMouvement;
     bool onPlatform;
+    int plateformCount;
     int controllerNumber;
+
+    public AudioClip atterissageHerbe;
+    public AudioClip atterissageBois;
+    public AudioClip atterissagePierre;
+    public AudioClip atterissageGlace;
 
 	// Use this for initialization
 	void Start () {
@@ -29,15 +35,29 @@ public class Input_Jump : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D coll)
     {
-        if (coll.tag != "prince") scriptMouvement.onPlatform[controllerNumber] = true;
-        scriptMouvement.animator.SetBool("isJumping", false);
-        //print(scriptMouvement.player);
         scriptMouvement.solType = coll.tag;
+        if (plateformCount <= 0) {
+            SoundStuff.PlayRandomOneShot(scriptMouvement.sourceSon, scriptMouvement.ClonkArmureMarche, 0.2f);
+
+            if(coll.tag == "Herbe")  scriptMouvement.sourceSon.PlayOneShot(atterissageHerbe,  0.3f);
+            if(coll.tag == "Bois")   scriptMouvement.sourceSon.PlayOneShot(atterissageBois,   0.3f);
+            if(coll.tag == "Glace")  scriptMouvement.sourceSon.PlayOneShot(atterissageGlace,  0.3f);
+            if(coll.tag == "Pierre") scriptMouvement.sourceSon.PlayOneShot(atterissagePierre, 0.3f);
+        }
+
+        if (coll.tag != "prince") {
+            scriptMouvement.onPlatform[controllerNumber] = true;
+            plateformCount ++;
+        }
+        scriptMouvement.animator.SetBool("isJumping", false);
     }
 
     void OnTriggerExit2D(Collider2D coll)
     {
-        if (coll.tag != "prince") scriptMouvement.onPlatform[controllerNumber] = false;
+        if (coll.tag != "prince") {
+            scriptMouvement.onPlatform[controllerNumber] = false;
+            plateformCount --;
+        }
         scriptMouvement.solType = "";
 
     }

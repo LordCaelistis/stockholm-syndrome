252ecdf3696c44af2f3b3575c6f3b83f057725ac
LordCaelistis <jules.seigneur77@gmail.com>
2018-12-17 23:10:09 +0100

    Images de victoire
    Ok donc normalement, j'ai rien cassé. J'ai trifouillé un affichage des images de victoire avec des Animator. Dans un sens, c'est plus propre. C'est pas à l'échelle, mais c'est plus propre. J'ai bossé dans la scène de Nicolas parce que je suis un gros sale et que j'ai oublié d'en sortir

--- Assets/Programmation/Timer.cs
+++ Assets/Programmation/Timer.cs
@@ -7,19 +7,16 @@ public class Timer : MonoBehaviour {
 
     public GameObject player1;
     public GameObject player2;
-    public static string hasPrince = null;
-    public static bool P1HasPrince = false;
-    public static bool P2HasPrince = false;
     public GameObject PrincePrefab;
     public static Dictionary<int, bool> playerArray = new Dictionary<int, bool>();
     public float[] playerScore;
     public float winningScore = 3f;
     public bool gameOver = false;
-    private int winningPlayer;
+    private int winningPlayer = 0;
     Rigidbody2D princeRigid;
 
-    public Image VictoryPlayer1;
-    public Image VictoryPlayer2;
+    public Animator VictoryPlayer1;
+    public Animator VictoryPlayer2;
 
     private int tempControllerNumber;
 
@@ -30,8 +27,8 @@ public class Timer : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        VictoryPlayer1.enabled = false;
-        VictoryPlayer2.enabled = false;
+        //VictoryPlayer1.enabled = false;
+        //VictoryPlayer2.enabled = false;
         if(textObject != null){
             textZone = textObject.GetComponent<Text>();
         }
@@ -47,23 +44,6 @@ public class Timer : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-        /*if (Input.GetKeyDown("e")) {
-            print(playerArray[1] + "Fire1");
-        }
-        if (Input.GetKeyDown("r")) {
-            print(playerArray[2] + "Fire2");
-        }
-        //Quand le Player1 se prend un gnon
-        if (Input.GetKeyDown("t") && P1HasPrince)
-        {
-            Vector3 pos0 = player1.transform.position;
-            Quaternion rot0 = Quaternion.identity;
-            GameObject prince0 = (GameObject)Instantiate(PrincePrefab, pos0, rot0);
-            princeRigid = prince0.GetComponent<Rigidbody2D>();
-            princeRigid.velocity += new Vector2(0, 8);
-            P1HasPrince = !P1HasPrince;
-        }*/
-
 
         foreach (KeyValuePair<int, bool> item in playerArray)
         {
@@ -91,8 +71,8 @@ public class Timer : MonoBehaviour {
             textZone.text = "P1 : " + playerScore[0] + " | P2 : " + playerScore[1];
         } else if (textZone != null && gameOver == true)
         {
-            if(winningPlayer == 1) VictoryPlayer1.enabled = true;
-            if(winningPlayer == 2) VictoryPlayer2.enabled = true;
+            if(winningPlayer == 1) VictoryPlayer1.SetFloat("VictoryTrigger", 1f);
+            if(winningPlayer == 2) VictoryPlayer2.SetFloat("VictoryTrigger2", 1f);
         }
     }
 }
